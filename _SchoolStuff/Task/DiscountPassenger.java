3e9a0f3d1fc5eb0c2ef1945d0d1297cc99c36919
Nathaniel-Ellacer <nathanielashleythampson@gmail.com>
2024-09-30 18:38:30 +0800

    Act 2

--- _SchoolStuff/Task/DiscountPassenger.java
+++ _SchoolStuff/Task/DiscountPassenger.java
@@ -1,4 +1,3 @@
-package _SchoolStuff.Task;
 
 import java.text.DecimalFormat;
 import java.util.Scanner;
@@ -8,65 +7,68 @@ public class DiscountPassenger {
         Scanner input = new Scanner(System.in);
         DecimalFormat df = new DecimalFormat("###,###.##");
 
-        char fareCode, yesOrNo;
-        double fare, discount, discountPercentage, businessCharge, newFare;
+        char fareCode;
+        double fare;
+        double discount;
+        double discountPercentage;
+        double businessCharge;
+        double newFare;
 
         businessCharge = 0.0;
         discountPercentage = 0.0;
-        
+
         System.out.print("Enter fare: ");
         fare = input.nextDouble();
 
         System.out.print("Passenger type [O: Ordinary, S: Student, C: Senior Citizen]: ");
-        fareCode = input.next().charAt(0);
+        fareCode = Character.toUpperCase(input.next().charAt(0));
 
         System.out.print("Travelling in business class? [Y/N]: ");
-        yesOrNo = input.next().charAt(0);
+        char businessClass = Character.toUpperCase(input.next().charAt(0));
 
-        //safety net
-        yesOrNo = Character.toUpperCase(yesOrNo);
-        fareCode = Character.toUpperCase(fareCode);
-        switch (yesOrNo) {
+        switch (businessClass) {
             case 'Y':
                 switch (fareCode) {
                     case 'O':
                         businessCharge = 1000;
                         break;
+
                     case 'S':
                         businessCharge = 700;
                         break;
+
                     case 'C':
                         businessCharge = 500;
                         break;
                 }
                 break;
-
-            case 'N': 
-                 businessCharge = 0;
-                break; 
+            case 'N':
+                businessCharge = 0.0;
+                break;
             default:
-                System.out.println("invalid input");
+                System.out.println("Invalid input");
         }
 
         switch (fareCode) {
             case 'O':
                 discountPercentage = 0.0;
                 break;
+
             case 'S':
                 discountPercentage = 0.05;
                 break;
+
             case 'C':
                 discountPercentage = 0.10;
                 break;
+
             default:
-                System.out.println("invalid input");
+                System.out.println("Invalid passenger type.");
         }
 
         discount = fare * discountPercentage;
         newFare = fare - discount + businessCharge;
 
-        System.out.println("\nDiscount: " + df.format(discount));
-        System.out.println("Bussiness class charge: " + df.format(businessCharge));
-        System.out.println("New fare: " + df.format(newFare));
+        System.out.println("\nDiscount: " + df.format(discount) + "\nBusiness class charge: " + df.format(businessCharge) + "\nNew fare: " + df.format(newFare));
     }
 }
