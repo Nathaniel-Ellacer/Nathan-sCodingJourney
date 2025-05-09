4f951233c00c01d6de493aa507c4fda433c8bc1a
Nathaniel-Ellacer <nathanielashleythampson@gmail.com>
2025-05-09 05:13:11 +0000

    commit

---
+++ FileSample/QuarterSales.java
@@ -0,0 +1,28 @@
+package FileSample;
+
+
+public class QuarterSales {
+    public static void main(String[] args)
+    {
+        openFile();
+        processFile();
+        closeFile();
+    }
+
+    public static void openFile()
+    {
+        Scanner input=new Scanner(System.in);
+
+        inputFile=new Scanner(new FileReader("https://github.com/Nathaniel-Ellacer/Nathan-sCodingJourney/blob/master/FileSample/SaleData.csv"))
+    }
+
+    public static void processFile()
+    {
+        
+    }
+
+    public static void closeFile()
+    {
+        
+    }
+}
